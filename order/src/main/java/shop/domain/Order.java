4d6f0e1cce0062afd87f29e25e9e81cb789295e7
Samuni <sanghoon01@uengine.org>
2023-10-10 13:54:35 +0900

    commit

---
+++ order/src/main/java/shop/domain/Order.java
@@ -0,0 +1,43 @@
+package shop.domain;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import lombok.Data;
+import shop.OrderApplication;
+import shop.domain.OrderCancelled;
+import shop.domain.OrderPlaced;
+
+@Entity
+@Table(name = "Order_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String devlieryId;
+
+    @PostPersist
+    public void onPostPersist() {
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+        OrderCancelled orderCancelled = new OrderCancelled(this);
+        orderCancelled.publishAfterCommit();
+    }
+
+    @PreRemove
+    public void onPreRemove() {}
+
+    public static OrderRepository repository() {
+        OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
+            OrderRepository.class
+        );
+        return orderRepository;
+    }
+}
+//>>> DDD / Aggregate Root
---
+++ supporting/src/main/java/shop/domain/DeliveryReturned.java
@@ -0,0 +1,24 @@
+package shop.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import shop.domain.*;
+import shop.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class DeliveryReturned extends AbstractEvent {
+
+    private Long id;
+
+    public DeliveryReturned(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryReturned() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
